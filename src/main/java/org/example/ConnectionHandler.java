73224fb764b878e563a42ee37ab6b5098545903a
Frossstik <lehapoh@gmail.com>
2023-10-30 01:26:08 +0300

    add

--- src/main/java/org/example/ConnectionHandler.java
+++ src/main/java/org/example/ConnectionHandler.java
@@ -2,23 +2,15 @@ package org.example;
 import java.io.*;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class ConnectionHandler {
+
     private static String HTTP_HEADERS_TYPE;
-
-    private static String HTTP_BODY;
-
-    static class JsonMessage{
-        public String message;
-    }
     private static final String HTTP_HEADERS = "HTTP/1.1 200 OK\n" +
             "Date: Mon, 18 Sep 2023 14:08:55 +0200\n" +
-            "HttpServer: Simple Webserver\n" +
-            "Content-Length: 180\n";
+            "HttpServer: Simple Webserver\n";
 
-    private static final String HTTP_BODY_HTML = "<!DOCTYPE html>\n"
-            +
+    private static final String HTTP_BODY_HTML = "<!DOCTYPE html>\n" +
             "<html lang=\"en\">\n" +
             "<head>\n" +
             "<meta charset=\"UTF-8\">\n" +
@@ -28,60 +20,68 @@ public class ConnectionHandler {
             "<h1>Hi!</h1>\n" +
             "<p>This is a simple line in html.</p>\n" +
             "</body>\n" +
-            "</html>\n" +
-            "\n";
+            "</html>\n";
+    private static String HTTP_BODY;
+    private static final String HTTP_BODY_TEXT = "TEXT\n";
 
-    private static final String HTTP_BODY_TEXT = "This is a simple line.";
+    private static final String HTTP_BODY_JSON = "{type: JSON}\n";
     private Socket socket;
+
     public ConnectionHandler(Socket socket) {
         this.socket = socket;
         handle();
     }
+
     public void handle() {
         try {
-            var inputStreamReader = new BufferedReader(
-                    new InputStreamReader(socket.getInputStream(),StandardCharsets.US_ASCII));
-            var outputStreamWriter = new BufferedWriter(
-                    new OutputStreamWriter(socket.getOutputStream(), StandardCharsets.US_ASCII));
-            if (parseRequest(inputStreamReader).contains("application/json")) {
-                ObjectMapper mapper = new ObjectMapper();
-                JsonMessage jsonMessage = new JsonMessage();
-                jsonMessage.message = HTTP_BODY_TEXT;
-                HTTP_HEADERS_TYPE = ("Content-Type: application/json\n");
-                HTTP_BODY = mapper.writeValueAsString(jsonMessage);
-                System.out.println(HTTP_HEADERS+HTTP_HEADERS_TYPE+HTTP_BODY);
-                writeResponse(outputStreamWriter);
+            var inputStreamReader = new BufferedReader(new InputStreamReader(socket.getInputStream(),
+                    StandardCharsets.UTF_8));
+            var outputStreamWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream(),
+                    StandardCharsets.UTF_8));
+            switch (parseRequest(inputStreamReader)) {
+                case "application/json": {
+                    HTTP_HEADERS_TYPE = "Content-Length: 12\n" +
+                            "Content-Type: application/json\n";
+                    HTTP_BODY = HTTP_BODY_JSON;
+                    writeResponse(outputStreamWriter);
+                }
+                break;
+                case "text/html": {
+                    HTTP_HEADERS_TYPE = "Content-Length: 180\n" +
+                            "Content-Type: text/html\n";
+                    HTTP_BODY = HTTP_BODY_HTML;
+                    writeResponse(outputStreamWriter);
+                }
+                break;
+                default: {
+                    HTTP_HEADERS_TYPE = "Content-Length: 4\n" +
+                            "Content-Type: text/plain\n" +
+                            "Content-Disposition: attachment; filename=file.txt\n";
+                    HTTP_BODY = HTTP_BODY_TEXT;
+                    writeResponse(outputStreamWriter);
+                }
+                break;
             }
-            else if (parseRequest(inputStreamReader).contains("text/plain" +
-                    "Content-Disposition: attachment; filename=File.txt\n")){
-                HTTP_HEADERS_TYPE = ("Content-Type: text/html\n");
-                HTTP_BODY = HTTP_BODY_TEXT;
-                System.out.println(HTTP_HEADERS+HTTP_HEADERS_TYPE+HTTP_BODY);
-                writeResponse(outputStreamWriter);
-            }
-            else {
-                HTTP_HEADERS_TYPE = ("Content-Type: text/html\n");
-                HTTP_BODY = HTTP_BODY_HTML;
-                System.out.println(HTTP_HEADERS+HTTP_HEADERS_TYPE+HTTP_BODY);
-                writeResponse(outputStreamWriter);
-            }
-        }
-         catch (IOException e) {
+        } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
+
     private String parseRequest(BufferedReader inputStreamReader) throws IOException {
         String str = "";
         var request = inputStreamReader.readLine();
         while (request != null && !request.isEmpty()) {
-            System.out.println(request);
+            if (request.contains("Accept:"))
+                str = request.substring(7).trim();
             request = inputStreamReader.readLine();
         }
         return str;
     }
+
     private void writeResponse(BufferedWriter outputStreamWriter) {
         try {
-            outputStreamWriter.write(HTTP_HEADERS + HTTP_HEADERS_TYPE);
+            outputStreamWriter.write(HTTP_HEADERS);
+            outputStreamWriter.write(HTTP_HEADERS_TYPE);
             outputStreamWriter.newLine();
             outputStreamWriter.write(HTTP_BODY);
             outputStreamWriter.newLine();
